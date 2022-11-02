357af6ce6f40391c71d4c169ebb602e0f08c9cfc
Bhumsitt Pramuanpornsatid <32533504+Bosebcc@users.noreply.github.com>
2022-11-01 23:34:06 -0500

    MP1: test1 - Fix Place.java search method

--- app/src/main/java/edu/illinois/cs/cs124/ay2022/mp/models/Place.java
+++ app/src/main/java/edu/illinois/cs/cs124/ay2022/mp/models/Place.java
@@ -1,5 +1,6 @@
 package edu.illinois.cs.cs124.ay2022.mp.models;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /*
@@ -64,7 +65,55 @@ public final class Place {
 
   // Search place
   public static List<Place> search(final List<Place> places, final String search) {
-    assert false;
-    return null;
+    // Check for null
+    if (places == null || search == null) {
+      throw new IllegalArgumentException();
+    }
+
+    // Trim, lowercase search string
+    String s = search.trim().toLowerCase();
+
+    // If search string empty return whole list unmodified
+    if (s.length() == 0) {
+      return places;
+    }
+
+    // New empty list for return
+    List<Place> filtered = new ArrayList<>();
+
+    // Find keyword(search string) from description of each places
+    for (Place place : places) {
+      // Format description:
+      // Swap out special character for space
+      String desc = place.description.toLowerCase();
+      desc = desc.replaceAll("\\.", " ");
+      desc = desc.replaceAll("!", " ");
+      desc = desc.replaceAll("\\?", " ");
+      desc = desc.replaceAll(",", " ");
+      desc = desc.replaceAll(":", " ");
+      desc = desc.replaceAll(";", " ");
+      desc = desc.replaceAll("/", " ");
+
+      // Remove none alphabetical character ; Keep alphabet, number, space
+      String descForm = "";
+      for (int i = 0; i < desc.length(); i++) {
+        boolean b1 = Character.isAlphabetic(desc.charAt(i));
+        boolean b2 = Character.isDigit(desc.charAt(i));
+        boolean b3 = Character.isSpaceChar(desc.charAt(i));
+        if (b1 || b2 || b3) {
+          descForm += desc.substring(i, i + 1);
+        }
+      }
+
+      // Match keyword to description
+      String[] words = descForm.split(" ");
+      for (String word : words) {
+        if (word.equals(s)) {
+          filtered.add(place);
+          break;
+        }
+      }
+    }
+    return filtered;
   }
 }
