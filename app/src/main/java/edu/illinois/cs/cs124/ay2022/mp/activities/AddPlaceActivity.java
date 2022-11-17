da87946dc0ba172266c7dc7c1629d145fb014c7a
Bhumsitt Pramuanpornsatid <32533504+Bosebcc@users.noreply.github.com>
2022-11-17 13:10:32 -0600

    MP2: Test 3: Implement new AddPlaceActivity when long press. User can now add new place on the map with custom description.

--- app/src/main/java/edu/illinois/cs/cs124/ay2022/mp/activities/AddPlaceActivity.java
+++ app/src/main/java/edu/illinois/cs/cs124/ay2022/mp/activities/AddPlaceActivity.java
@@ -1,5 +1,80 @@
 package edu.illinois.cs.cs124.ay2022.mp.activities;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import edu.illinois.cs.cs124.ay2022.mp.R;
+import edu.illinois.cs.cs124.ay2022.mp.application.FavoritePlacesApplication;
+import edu.illinois.cs.cs124.ay2022.mp.models.Place;
+import edu.illinois.cs.cs124.ay2022.mp.models.ResultMightThrow;
+import edu.illinois.cs.cs124.ay2022.mp.network.Client;
+import java.util.concurrent.CompletableFuture;
 
-public class AddPlaceActivity extends AppCompatActivity {}
+public class AddPlaceActivity extends AppCompatActivity {
+  private static final String TAG = AddPlaceActivity.class.getSimpleName();
+
+  // Helper function for posting place to server
+  private void clientPostPlace(final Place place) {
+    final Client client = Client.start();
+    // A CompletableFuture allows us to wait for the result of an asynchronous call
+    CompletableFuture<ResultMightThrow<Boolean>> completableFuture = new CompletableFuture<>();
+    // When getPlaces returns, it causes the CompletableFuture to complete
+    client.postFavoritePlace(place, completableFuture::complete);
+  }
+
+  // Activity Start
+  @Override
+  protected void onCreate(@Nullable final Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    // Load the layout for this activity
+    setContentView(R.layout.activity_addplace);
+
+    // Get this activity intent and value passed through extra
+    Intent intent = getIntent();
+    String latitude = intent.getStringExtra("latitude");
+    String longitude = intent.getStringExtra("longitude");
+    double lat = Double.parseDouble(latitude);
+    double lon = Double.parseDouble(longitude);
+
+    // Define Intent for returning to MainActivity.java
+    // Set flag:
+    // This will cause any existing task that would be associated with
+    // the activity to be to IS cleared before the activity is started.
+    // That is, the activity becomes the new root of an otherwise empty task,
+    // and any old activities are finished.
+    Intent launchMainActivity = new Intent(this, MainActivity.class);
+    launchMainActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+
+    // Find the button component in the layout and configure listener
+    // Also save the reference for later use
+    Button cancelButton = findViewById(R.id.cancel_button);
+    cancelButton.setOnClickListener(
+        v -> {
+          Log.d(TAG, "Cancel button clicked");
+          startActivity(launchMainActivity);
+        });
+    Button saveButton = findViewById(R.id.save_button);
+    saveButton.setOnClickListener(
+        v -> {
+          Log.d(TAG, "Save button clicked: " + lat + " | " + lon);
+          EditText testWidget = findViewById(R.id.description);
+          String description = testWidget.getText().toString();
+          String name = "Test AddPlaceActivity"; // Set temporally name
+          String id = FavoritePlacesApplication.CLIENT_ID;
+
+          // Init new place
+          Place place = new Place(id, name, lat, lon, description);
+          // Try posting place to server using helper function
+          try {
+            clientPostPlace(place);
+          } catch (Exception e) {
+            Log.e(TAG, "Error posting place to client: " + e);
+          }
+          startActivity(launchMainActivity);
+        });
+  }
+}
--- app/src/main/java/edu/illinois/cs/cs124/ay2022/mp/application/FavoritePlacesApplication.java
+++ app/src/main/java/edu/illinois/cs/cs124/ay2022/mp/application/FavoritePlacesApplication.java
@@ -24,7 +24,7 @@ public final class FavoritePlacesApplication extends Application {
   public static final String SERVER_URL = "http://localhost:" + DEFAULT_SERVER_PORT + "/";
 
   // Put your ID (from ID.txt) here
-  public static final String CLIENT_ID = "";
+  public static final String CLIENT_ID = "61431790-b07b-4d2a-bb89-088d51e2068b";
 
   // Reference to our API client, which would potentially be used by multiple app activities
   private Client client;
